832631c93e0be8ecb88c43f693415829031f439b
Pedro Oliveira <pedrodepaivasoaresll@gmail.com>
2019-11-20 23:09:46 -0300

    create controller for plans

---
+++ src/app/controllers/PlanController.js
@@ -0,0 +1,19 @@
+class PlanController {
+  async store(req, res) {
+    return res.json();
+  }
+
+  async update(req, res) {
+    return res.json();
+  }
+
+  async destroy(req, res) {
+    return res.json();
+  }
+
+  async show(req, res) {
+    return res.json();
+  }
+}
+
+export default new PlanController();
