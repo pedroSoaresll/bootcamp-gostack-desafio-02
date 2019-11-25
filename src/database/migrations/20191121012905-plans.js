4bce86d9bab3316213a7cf65c378041974b3c4e3
Pedro Oliveira <pedrodepaivasoaresll@gmail.com>
2019-11-25 18:54:32 -0300

    fix plans down

--- src/database/migrations/20191121012905-plans.js
+++ src/database/migrations/20191121012905-plans.js
@@ -24,13 +24,5 @@ module.exports = {
     });
   },
 
-  down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.dropTable('users');
-    */
-  },
+  down: queryInterface => queryInterface.dropTable('plans'),
 };
